89f2d1adb2f9f95c7b23829a5b096e4fd73ae36a
emanuelklado <emanuelklado@msn.com>
2022-01-25 17:59:31 -0300

    Requisito ok, depois de muita abstraçao

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((manager) => manager.managers.includes(id));
 }
+// teste com manager falso
+// console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const objEmpregado = employees.filter((element) => element.managers.includes(managerId));
+    const array = [];
+    objEmpregado.forEach((element) =>
+      array.push(`${element.firstName} ${element.lastName}`));
+    return array;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
