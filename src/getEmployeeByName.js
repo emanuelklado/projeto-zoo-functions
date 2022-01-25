8af1023a4acd6713f36e59786b2ca5a9131ffc31
emanuelklado <emanuelklado@msn.com>
2022-01-25 16:49:22 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,13 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-return employees.
+  if (employeeName === undefined) {
+    return {};
+  }
+  const empregado = employees.find((pessoa) =>
+    pessoa.firstName === employeeName || pessoa.lastName === employeeName);
+  return empregado;
 }
 
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
